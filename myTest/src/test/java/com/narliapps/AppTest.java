a7b4c1bcc8a3a3e5ed6ddb7de4d5d3fdedc572b3
NOAH ARLISS <narliss@gmail.com>
2013-04-29 09:14:04 -0400

    Submit initial test project

---
+++ myTest/src/test/java/com/narliapps/AppTest.java
@@ -0,0 +1,74 @@
+/*
+ * File: AppTest.java
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * The contents of this file are subject to the terms and conditions of
+ * the Common Development and Distribution License 1.0 (the "License").
+ *
+ * You may not use this file except in compliance with the License.
+ *
+ * You can obtain a copy of the License by consulting the LICENSE.txt file
+ * distributed with this file, or by consulting
+ * or https://oss.oracle.com/licenses/CDDL
+ *
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file LICENSE.txt.
+ *
+ * MODIFICATIONS:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ */
+
+package com.narliapps;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest(String testName)
+    {
+        super(testName);
+    }
+
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite(AppTest.class);
+    }
+
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        try
+        {
+            Thread.sleep(5000);
+        }
+        catch (InterruptedException e)
+        {
+            e.printStackTrace();    // To change body of catch statement use File | Settings | File Templates.
+        }
+
+        assertTrue(true);
+    }
+}
